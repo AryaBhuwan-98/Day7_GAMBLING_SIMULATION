7246dc7cee37bf48118eaa09d795f57a3fcc34e0
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-18 22:51:33 -0800

    Add:WELCOME TO THE GAMBLING SIMULATION Programs

---
+++ src/main/java/com/bridgelabz/gamblingsimulation/App.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.gamblingsimulation;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "WELCOME TO GAMBLING SIMULATION PROBLEMs" );
+    }
+}
